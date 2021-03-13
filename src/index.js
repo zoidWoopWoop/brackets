7ba265182d70f9c80698f005ca3355cfd96c1848
zoidWoopWoop <NichiporWoopWoop@gmail.com>
2021-03-13 10:35:27 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = {}
+    let stack = []
+    for (const iterator of bracketsConfig) {
+        brackets[iterator[0]] = iterator[1];
+    }
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == brackets[stack[stack.length - 1]]) {
+            stack.pop()
+        } else {
+            stack.push(str[i])
+        }
+    }
+    return stack.length == 0;
 }
